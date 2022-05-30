d75d0f4f36a7a2f03abc04f516dc9eccfc1011c7
Pavel <p.yudenko@gmail.com>
2022-05-30 14:45:44 +0300

    refactoring

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -1,32 +1,6 @@
 #!/usr/bin/env node
-import processGenerator, { getRandomNumber } from '../src/index.js';
+import processGenerator from '../src/index.js';
+import { questionBlockGenerator, taskCondition } from '../src/progression.js';
 
-const taskCondition = 'What number is missing in the progression?';
-const questionBlockGenerator = () => {
-  // generate progression
-  const progression = [];
-  let firstNumber = getRandomNumber();
-  // progression counter is aqual 1/10 of random number
-  const progressionCounter = Math.ceil(getRandomNumber() / 10);
-  progression.push(firstNumber);
-  let iterCount = 10;
-  while (iterCount > 0) {
-    progression.push(progressionCounter + firstNumber);
-    firstNumber = progressionCounter + firstNumber;
-    iterCount -= 1;
-  }
-  // generate array with hidden char
-  const hiddenChar = [];
-  const getHiddenChar = () => {
-    // random position is aqual 1/10 of random number
-    const randomPosition = Math.floor(getRandomNumber() / 10);
-    hiddenChar.push(progression[randomPosition]);
-    progression[randomPosition] = '..';
-    return progression.join(' ');
-  };
-  // generate Q/A constants
-  const question = getHiddenChar(progression);
-  const [trueAnswer] = hiddenChar;
-  return [`${question}`, `${trueAnswer}`];
-};
+questionBlockGenerator();
 processGenerator(taskCondition, questionBlockGenerator);
