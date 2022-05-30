568fdc654e22da9b3cc10b8bb684c7e6dc34d1bb
Pavel <p.yudenko@gmail.com>
2022-05-30 14:09:43 +0300

    update comments, update content

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -6,20 +6,22 @@ const questionBlockGenerator = () => {
   // generate progression
   const progression = [];
   let firstNumber = getRandomNumber();
+  // progression counter is aqual 1/10 of random number
   const progressionCounter = Math.ceil(getRandomNumber() / 10);
   progression.push(firstNumber);
-  let i = 0;
-  while (i < 10) {
+  let iterCount = 10;
+  while (iterCount < 10) {
     progression.push(progressionCounter + firstNumber);
     firstNumber = progressionCounter + firstNumber;
-    i += 1;
+    iterCount -= 1;
   }
   // generate array with hidden char
   const hiddenChar = [];
   const getHiddenChar = () => {
+    // random position is aqual 1/10 of random number
     const randomPosition = Math.floor(getRandomNumber() / 10);
     hiddenChar.push(progression[randomPosition]);
-    progression[randomPosition] = '*';
+    progression[randomPosition] = '..';
     return progression.join(' ');
   };
   // generate Q/A constants
